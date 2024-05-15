24ba7e06d7cde20ea60b1ba316d80370c40c97d9
Megapixar86 <lysenyi_n@bk.ru>
2024-05-15 15:44:02 +0300

    добавлена функция спецификации, печать и структурной схема. В html добавлена таблицы спецификации. Изменил стили

--- script.js
+++ script.js
@@ -8,9 +8,7 @@
 	let total_power
 	let count  = 1
 	let power_zone = []
-	let panel = 0
-	//let panel = +el('panel').value
-
+	let panel = 1
 	//ограничить значение
 	function LimNum(num, minNum, maxNum){
 		if(num < minNum || isNaN(num) ){
@@ -29,10 +27,9 @@
 	}
 	//функция добавления зон
 	function addzone(){
-		//count = el('quantity').value
 		if(count<5){
 			count ++
-			let elem = '<div id="power_level'+count+'"><label for="zone" >Введите мощность, Вт (зона '+ count+'): </label><input type="text" id="power'+count+'" value="12" name="zone" size="1"><label for="zone">(от 12 до 520)</label></div>'
+			let elem = '<div id="power_level'+count+'"><label for="zone" >Введите мощность, Вт (линия '+ count+'): </label><input type="text" id="power'+count+'" value="12" name="zone" size="1"><label for="zone">(от 12 до 520)</label></div>'
 			es('#power_field').insertAdjacentHTML('beforeend', elem)
 			el('quantity').value = count
 		}
@@ -46,55 +43,96 @@
 			el('quantity').value = count
 		}
 	}
-	//подбор оборудования
-	function spec(){
-		UpdateData()
-		//let count = el('quantity').value
-		//let pow = UpdateData()
-		//panel = GetSetData('panel', 0, 32)
-		total_power = 0
-		for(let i =0; i<=count-1; i++){
-			total_power = total_power + power_zone[i]
-		}
-		console.log(total_power)
-		console.log(power_zone)
-		console.log(panel)
-		//if(power<200){
-		//}
-	}
+	//обновить данные ввода пользователя
 	function UpdateData(){
 		power_zone  = []
-		panel = GetSetData('panel', 0, 32)
-		//count  = el('quantity').value
-		//let count = el('quantity').value
-		//let power = []
-		//power[0] = GetSetData('count', 0, 32)
+		total_power = 0
+		panel = GetSetData('panel', 1, 5)
+		//получим мощности каждой зоны и расчитаем сумму
 		for(let i =1; i<=count; i++){
 			power_zone[i-1] = GetSetData('power'+ i, 12, 520)
-			//total_power = total_power + power_zone[i]
+			total_power = total_power + power_zone[i-1]
 		}
-		//console.log(power[0])
-		//return power
+		//добавим на страницу
+		el('total_power').value = total_power
 	}
-	// функция вставки данных на страницу
-	function getTable(href_model, name, descript, href){
-		let str = '<div id = "row"><a href=' + href_model + '>' + name + '</a></div><div id = "row">' + descript + '</div><div id = "row"><a href=' + href + '>support.luis.ru</a></div>'
-		es('#grid').insertAdjacentHTML('beforeend', str)
-		el("data").style.display = "block"
+	//функция выключения таблицы
+	function turnoff(){
+		el("head").style.display = "none"
+		el("LX240").style.display = "none"
+		el("DT1240").style.display = "none"
+		el("LX480").style.display = "none"
+		el("DTM1255L").style.display = "none"
+		el("LX650").style.display = "none"
+		el("DT1240").style.display = "none"
+		el("M1").style.display = "none"
+		el("btn2").style.display = "none"
+		el("btn3").style.display = "none"
+		el("empty").style.display = "none"
 	}
-	// функция вставки пустой таблицы данных на страницу
-	function getEmpty(){
-		let str = '<div id = "row"></div><div id = "row">нет подходящих систем</div><div id = "row"></div>'
-		es('#grid').insertAdjacentHTML('beforeend', str)
-		el("data").style.display = "block"
+	//подбор оборудования
+	function spec(){
+		turnoff()
+		UpdateData()
+		switch (true) {
+			case (total_power <= 200):
+				//console.log( '<200' );
+				el("head").style.display = "block"
+				el("LX240").style.display = "block"
+				if(el('alarm').checked){
+					el("DT1240").style.display = "block"
+				}
+				el("M1").style.display = "block"
+				el("row_q").innerHTML = panel
+				el("btn2").style.display = "block"
+				el("btn3").style.display = "block"
+				break;
+			case (total_power <= 400 &&  total_power >= 200):
+				//console.log( '>200 и <400' );
+				el("head").style.display = "block"
+				el("LX480").style.display = "block"
+				if(el('alarm').checked){
+					el("DTM1255L").style.display = "block"
+				}
+				el("M1").style.display = "block"
+				el("row_q").innerHTML = panel
+				el("btn2").style.display = "block"
+				el("btn3").style.display = "block"
+				break;
+			case (total_power <= 540 &&  total_power >= 400):
+				el("head").style.display = "block"
+				//console.log( '>400 и <540' );
+				el("LX650").style.display = "block"
+				if(el('alarm').checked){
+					el("DT1265").style.display = "block"
+				}
+				el("M1").style.display = "block"
+				el("row_q").innerHTML = panel
+				el("btn2").style.display = "block"
+				el("btn3").style.display = "block"
+				break;
+			default:
+				//console.log( 'Мощность превышает допустимую' );
+				el("empty").style.display = "block"
+		}
+		
 	}
+	// функция печати
+	function print() {
+		html2pdf( es('body') );
+	}
+	// функция перенаправления на структурную схему
+	const url = ()=> window.open("https://support.luis.ru/ru/home/LPA/lpaLX")
 	// основная функция
 	function onLoadHandler() {
 		//-- подключаем обработчик щелчка
 		el("btn").addEventListener("click", spec);
 		el("plus").addEventListener("click", addzone);
 		el("minus").addEventListener("click", killzone);
-		document.addEventListener("click", UpdateData)
+		document.addEventListener("click", UpdateData);
+		el("btn2").addEventListener("click", print);
+		el("btn3").addEventListener("click", url);
 	}
 	//пуск
+	document.addEventListener("DOMContentLoaded", UpdateData)
 	window.onload = onLoadHandler;
